fb74112f79cd5b88953edc26875978ea0d931f53
soonseong.k <taetae8939@gmail.com>
2024-01-24 20:02:39 +0900

    feat : 스틱 이동 구현

--- Assets/_Script/PlayerController.cs
+++ Assets/_Script/PlayerController.cs
@@ -1,18 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Camera _camera;
+
+    private void Awake()
     {
-        
+        _camera = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void OnAim(InputValue value)
     {
-        
+        Vector2 newAim = value.Get<Vector2>();
+        Vector2 worldPos = _camera.ScreenToWorldPoint(newAim);
+        MoveStick(new Vector2(worldPos.x, -4));
+    }
+
+    private void MoveStick(Vector2 pos)
+    {
+        // 위치 이동. 일단은 순간이동
+        gameObject.transform.position = pos;
     }
 }
