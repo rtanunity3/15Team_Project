83023927c8f12017d0c56ddcc4dfdfdb0484a71d
soonseong.k <taetae8939@gmail.com>
2024-01-25 14:01:13 +0900

    feat : 과일 active false

--- Assets/_Script/Fruit.cs
+++ Assets/_Script/Fruit.cs
@@ -7,5 +7,12 @@ public class Fruit : MonoBehaviour
     // 프리펩 하나로 해서 세팅하려했으나
     // 과일 종류별로 프리펩을 만드는게 오버헤드가 적은것 같아 프리펩으로 해결
 
-    
+    private void Update()
+    {
+
+        if (gameObject.transform.position.y < -8)
+        {
+            gameObject.SetActive(false);
+        }
+    }
 }
